40a4e8ce7da990cb541bcc7a6ffcd99fb22bbb4b
psj98 <park98sj1@hanmail.net>
2021-01-23 23:33:32 +0900

    update

---
+++ src/useInput.js
@@ -0,0 +1,21 @@
+export const useInput = (initialValue, validator) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = (event) => {
+        const {
+            target: { value }
+        } = event;
+
+        let willUpdate = true;
+
+        // validator는 함수이므로 if문 실행
+        if (typeof validator === "function") {
+            willUpdate = validator(value); // value의 유효성 검증
+        }
+
+        if (willUpdate) {
+            setValue(value);
+        }
+    };
+
+    return { value, onChange };
+};
